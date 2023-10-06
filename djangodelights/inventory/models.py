0b1cff1f74baea0763442267cc49c7241af54b9f
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-06 16:28:40 -0400

    updated models

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -4,13 +4,14 @@ from django.db import models
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
     quantity = models.PositiveIntegerField()
-    price_per_unit = models.DecimalField(max_digits=10, decimal_places=2)
+    unit = models.CharField(max_length=255)
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.name
     
 class MenuItem(models.Model):
-    name = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
@@ -19,14 +20,14 @@ class MenuItem(models.Model):
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
-    quantity_required = models.PositiveIntegerField()
+    quantity = models.PositiveIntegerField()
 
     def __str__(self):
         return f"{self.menu_item.name} requires {self.quantity_required} of {self.ingredient.name}"
     
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    purchase_time = models.DateTimeField(auto_now_add=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Purchased {self.menu_item.name} at {self.purchase_time}"
