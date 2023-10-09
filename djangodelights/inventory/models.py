703073c94b34a59d539d4653fd459ab7d71c0bfc
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-09 15:15:40 -0400

    created views

--- djangodelights/inventory/models.py
+++ djangodelights/inventory/models.py
@@ -15,7 +15,7 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
-        return self.name
+        return self.title
     
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
@@ -23,11 +23,11 @@ class RecipeRequirement(models.Model):
     quantity = models.PositiveIntegerField()
 
     def __str__(self):
-        return f"{self.menu_item.name} requires {self.quantity_required} of {self.ingredient.name}"
+        return f"{self.menu_item.title} requires {self.quantity} of {self.ingredient.name}"
     
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"Purchased {self.menu_item.name} at {self.purchase_time}"
+        return f"Purchased {self.menu_item.title} at {self.timestamp}"
--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 # Create your views here.
+
+class InventoryView(TemplateView):
+    template_name = 'inventory.html'
+
+    def get_context_data(self, **kwargs)
+        context = super().get_context_data(**kwargs)
+        context['ingredients'] = Ingredient.objects.all()
+        return context
+    
+class PurchaseView(TemplateView):
+    template_name = 'purchase.html'
+
+    def get_contest_data(self, **kwargs)
+        context = super().get_context_data(**kwargs)
+        context['purchases'] = Purchase.objects.all()
+        return context
+    
+class MenuView(TemplateView):
+    template_name = 'menu.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu_items'] = MenuItem.objects.all()
+        return context
+    
+class FinancialsView(TemplateView):
+    template_name = 'financials.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        total_revenue = Purchase.objects.aggregate(total=Sum('menu_item__price'))['total']
+        total_cost = sum([req.ingredient.unit_price * req.quantity for purchase in Purchase.objects.all() for req in RecipeRequirement.objects.filter(menu_item=purchase.menu_item)])
+        context['total_revenue'] = total_revenue
+        context['total_cost'] = total_cost
+        context['profit'] = total_revenue - total_cost
+        return context
