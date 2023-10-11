ca37c2f412c1a1676b609f38e5b39cede4bc4409
Margaret Camilletti <mhcamilletti@gmail.com>
2023-10-11 16:16:56 -0400

    created forms

--- djangodelights/inventory/views.py
+++ djangodelights/inventory/views.py
@@ -1,10 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import IngredientForm, PurchaseForm, MenuItemForm, RecipeRequirementForm
+from .models import Ingredient, RecipeRequirement
 from django.urls import reverse_lazy
 from django.db.models import Sum
 from django.views.generic import TemplateView
 from django.views.generic.edit import DeleteView
 from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 # Create your views here.
+
+def add_ingredient(request):
+    if request.method == 'POST':
+        form = IngredientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory/inventory.html')
+    else: 
+        form = IngredientForm()
+    return render(request, 'add_ingredient.html', {'form': form})
+
+def purchase_menu_item(request):
+    if request.method == 'POST':
+        form = PurchaseForm(request.POST)
+        if form.is_valid():
+            menu_item = form.cleaned_data.get('menu_item')
+            requirements = RecipeRequirement.objects.filter(meneu_item=menu_item)
+            for req in requirements:
+                ingredient = req.ingredient
+                if ingredient.quantity < req.quantity:
+                    return render(request, 'error.html', {'message': 'Not enought ingredients'})
+                ingredient.quantity -= req.quantity
+                ingredient.save()
+            form.save()
+            return redirect('inventory/menu.html')
+        else:
+            form = PurchaseForm()
+        return render(request, 'purchase_menu_item.html', {'form': form})
+    
+def add_menu_item(request):
+    if request.method == 'POST':
+        form = MenuItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory/menu.html') 
+    else:
+        form = MenuItemForm()
+    return render(request, 'add_menu_item.html', {'form': form})
+
+def add_recipe_requirement(request):
+    if request.method == 'POST':
+        form = RecipeRequirementForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('inventory/inventory.html')
+    else:
+        form = RecipeRequirementForm()
+    return render(request, 'add_recipe_requirement.html', {'form': form})
 
 class InventoryView(TemplateView):
     template_name = 'inventory/inventory.html'
